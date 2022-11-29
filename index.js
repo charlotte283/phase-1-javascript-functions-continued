f3a2c09fb4e55d8f6678399fa2cc2658f4422582
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-11-29 16:42:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+let mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+let wrapAdjective = function (visualFlair='*') {
+    return function (adjective="special"){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
